4b21f2d6836e759a5fa42a93f3eb8b511f1db9b9
Nikos Roussos <nikos@roussos.cc>
2016-03-01 17:15:53 +0200

    Add about and map

---
+++ static/js/map.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    L.mapbox.accessToken = 'pk.eyJ1IjoiY29temVyYWRkIiwiYSI6ImxjQjFHNFUifQ.ohrYy34a8ZIZejrPSMWIww';
+    var map = L.mapbox.map('map', 'comzeradd.p9k5a39m').setView([37.98747, 23.73499], 17);
+});
