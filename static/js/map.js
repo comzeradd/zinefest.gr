d2fb2c41e92a93f98ecd8340b31893902a812bbf
Nikos Roussos <nikos@roussos.cc>
2017-04-27 10:46:12 +0300

    Fix map location

--- static/js/map.js
+++ static/js/map.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
     L.mapbox.accessToken = 'pk.eyJ1IjoiY29temVyYWRkIiwiYSI6ImxjQjFHNFUifQ.ohrYy34a8ZIZejrPSMWIww';
-    var map = L.mapbox.map('map', 'comzeradd.p9k5a39m').setView([37.98747, 23.73499], 17);
+    var map = L.mapbox.map('map', 'comzeradd.p9k5a39m').setView([37.9734, 23.7149], 15);
+
+    var marker = L.marker([37.9734, 23.7149], {
+        clickable: false
+    });
+
+    marker.addTo(map);
 });
